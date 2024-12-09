4f3bee8722c9633d9a10fcac178cff8dd015aa46
KalandarovJamshid01 <kalandarovjamshid01@gmail.com>
2024-12-09 15:33:40 +0500

    adresss basics

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,4 +3,18 @@ import { Column } from 'typeorm';
 export class User {
   @Column({ type: 'date', nullable: true })
   birth_date: Date;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  birth_place: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  pport_no: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  user_id: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  ctzn: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  email: string;
 }
