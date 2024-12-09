930033b83d101ef661038490f42a598fe4c5280b
KalandarovJamshid01 <kalandarovjamshid01@gmail.com>
2024-12-09 15:34:43 +0500

    adress entity was updated

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -17,4 +17,48 @@ export class User {
 
   @Column({ type: 'varchar', length: 255, unique: true })
   email: string;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  first_name: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  full_name: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  gd: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  mid_name: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  mob_phone_no: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  natn: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  per_adr: string;
+
+  @Column({ type: 'bigint', nullable: false, unique: true })
+  pin: number;
+
+  @Column({ type: 'date', nullable: true })
+  pport_expr_date: Date;
+
+  @Column({ type: 'date', nullable: true })
+  pport_issue_date: Date;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  pport_issue_place: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  sur_name: string;
+
+  @Column({ type: 'int', nullable: true })
+  tin: number;
+
+  @Column({ type: 'enum', enum: ['I', 'L'], nullable: true })
+  user_type: 'I' | 'L';
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  password: string;
 }
