15f7842fedd17c8d4eaa949a50a25ce82c0b8e75
KalandarovJamshid01 <kalandarovjamshid01@gmail.com>
2024-12-30 17:29:58 +0500

    deliver_pricing service updated

--- src/modules/delivery_pricing/delivery_pricing.service.ts
+++ src/modules/delivery_pricing/delivery_pricing.service.ts
@@ -5,22 +5,17 @@ import { UpdateDeliveryPricingDto } from './dto/update-delivery_pricing.dto';
 @Injectable()
 export class DeliveryPricingService {
   create(createDeliveryPricingDto: CreateDeliveryPricingDto) {
-    return 'This action adds a new deliveryPricing';
   }
 
   findAll() {
-    return `This action returns all deliveryPricing`;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} deliveryPricing`;
   }
 
   update(id: number, updateDeliveryPricingDto: UpdateDeliveryPricingDto) {
-    return `This action updates a #${id} deliveryPricing`;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} deliveryPricing`;
   }
 }
